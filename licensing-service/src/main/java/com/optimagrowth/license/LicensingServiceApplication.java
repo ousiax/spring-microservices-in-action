62f4f0c9240e9f3a5edb524fcad8773d2fe5018d
Jon.X <ousiax@hotmail.com>
2023-12-31 23:01:55 +0800

    Building microservices with Spring Boot

--- licensing-service/src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
+++ licensing-service/src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
@@ -1,7 +1,13 @@
 package com.optimagrowth.license;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class LicensingServiceApplication {
@@ -10,4 +16,18 @@ public class LicensingServiceApplication {
 		SpringApplication.run(LicensingServiceApplication.class, args);
 	}
 
+    @Bean
+    LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setUseCodeAsDefaultMessage(true);
+		messageSource.setBasenames("messages");
+		return messageSource;
+	}
 }
