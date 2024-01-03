0f208c787ae4c46ee470310c09885d6d04278b8c
Jon.X <ousiax@hotmail.com>
2024-01-03 18:09:24 +0800

    Service routing with Spring Cloud Gateway

---
+++ gatewayserver/src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
@@ -0,0 +1,34 @@
+package com.optimagrowth.gateway.filters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Configuration
+public class ResponseFilter {
+
+    @Autowired
+    FilterUtils filterUtils;
+
+    @Bean
+    public GlobalFilter postGlobalFilter() {
+        return (exchange, chain) -> {
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+                String correlationId = filterUtils.getCorrelationId(requestHeaders);
+
+                log.debug("Adding the correlation id to the outbound headers. {}", correlationId);
+
+                exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
+
+                log.debug("Completing outgoing request for {}.", exchange.getRequest().getURI());
+            }));
+        };
+    }
+}
--- licensing-service/src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
+++ licensing-service/src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
@@ -1,5 +1,8 @@
 package com.optimagrowth.license;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,7 +11,10 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
+
+import com.optimagrowth.license.utils.UserContextInterceptor;
 
 @SpringBootApplication
 @RefreshScope
@@ -37,7 +43,15 @@ public class LicensingServiceApplication {
 
 	@LoadBalanced
 	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+	RestTemplate getRestTemplate() {
+		RestTemplate template = new RestTemplate();
+		List<ClientHttpRequestInterceptor> interceptors = template.getInterceptors();
+		if (interceptors == null) {
+			template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+		} else {
+			interceptors.add(new UserContextInterceptor());
+			template.setInterceptors(interceptors);
+		}
+		return template;
 	}
 }
