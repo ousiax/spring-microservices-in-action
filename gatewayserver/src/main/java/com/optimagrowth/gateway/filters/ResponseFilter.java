f70d64eb6bc215da66a7ab30597667d26aeff51f
Jon.X <ousiax@hotmail.com>
2024-01-21 11:37:01 +0800

    Add io.micrometer

--- gatewayserver/src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
+++ gatewayserver/src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 
+import io.micrometer.tracing.Tracer;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
@@ -14,21 +14,49 @@ import reactor.core.publisher.Mono;
 public class ResponseFilter {
 
     @Autowired
+    Tracer tracer;
+
+    @Autowired
     FilterUtils filterUtils;
 
     @Bean
     public GlobalFilter postGlobalFilter() {
         return (exchange, chain) -> {
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
-                String correlationId = filterUtils.getCorrelationId(requestHeaders);
+            return chain.filter(exchange)
+                    .then(Mono.fromRunnable(() -> {
+                        String traceId = tracer.currentSpan()
+                                .context()
+                                .traceId();
+                        log.debug("Adding the correlation id to the outbound headers. {}", traceId);
 
-                log.debug("Adding the correlation id to the outbound headers. {}", correlationId);
+                        exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, traceId);
 
-                exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
-
-                log.debug("Completing outgoing request for {}.", exchange.getRequest().getURI());
-            }));
+                        log.debug("Completing outgoing request for {}.", exchange.getRequest().getURI());
+                    }));
         };
     }
 }
+
+// @Slf4j
+// @Configuration
+// public class ResponseFilter {
+
+//     @Autowired
+//     FilterUtils filterUtils;
+
+//     @Bean
+//     public GlobalFilter postGlobalFilter() {
+//         return (exchange, chain) -> {
+//             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+//                 HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+//                 String correlationId = filterUtils.getCorrelationId(requestHeaders);
+
+//                 log.debug("Adding the correlation id to the outbound headers. {}", correlationId);
+
+//                 exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
+
+//                 log.debug("Completing outgoing request for {}.", exchange.getRequest().getURI());
+//             }));
+//         };
+//     }
+// }
